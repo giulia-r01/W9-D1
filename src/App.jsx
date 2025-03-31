2d1f988e90c082e299cdae97d0a2b8ecbea6ed86
Giulia Rizzo <girzzo@gmail.com>
2025-03-31 15:00:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <h1>W9-D1</h1>
+      <h2>Proviamo a fare l'esercizio</h2>
       <ImageComponent
         src="https://external-preview.redd.it/82b2SEhz_8N-ibLQvE8G751FO5lSdypgVHjhaAGnYTM.jpg?auto=webp&s=0bf6f6d4b1a4497aacc3596bc6451c45edbfd65d"
         alt="Panda rosso"
