17fc2bbfccaf1c17f573c3999d1a5064a5e0fd92
Giulia Rizzo <girzzo@gmail.com>
2025-03-31 15:08:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,23 @@ function App() {
 
   return (
     <>
-      <h1>W9-D1</h1>
-      <h2>Proviamo a fare l'esercizio</h2>
-      <ImageComponent
-        src="https://external-preview.redd.it/82b2SEhz_8N-ibLQvE8G751FO5lSdypgVHjhaAGnYTM.jpg?auto=webp&s=0bf6f6d4b1a4497aacc3596bc6451c45edbfd65d"
-        alt="Panda rosso"
-        width="400"
-      />
-      <ButtonComponent text="Checcarino" />
+      <header>
+        <h1>W9-D1</h1>
+      </header>
+      <main>
+        <h2>Proviamo a fare l'esercizio</h2>
+        <ImageComponent
+          src="https://external-preview.redd.it/82b2SEhz_8N-ibLQvE8G751FO5lSdypgVHjhaAGnYTM.jpg?auto=webp&s=0bf6f6d4b1a4497aacc3596bc6451c45edbfd65d"
+          alt="Panda rosso"
+          width="400"
+        />
+        <p>
+          Il panda rosso (Ailurus fulgens) è un mammifero erbivoro di dimensioni
+          relativamente piccole, simile a un grosso gatto. È un animale timido e
+          schivo, che vive nelle foreste dell'Asia.
+        </p>
+        <ButtonComponent text="Scopri di più" />
+      </main>
     </>
   )
 }
